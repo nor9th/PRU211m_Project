82b4c90907edf657cb4b66c014d07d3933d2d9a4
DuctmHE153518 <97203297+DuctmHE153518@users.noreply.github.com>
2023-03-04 01:00:42 +0700

    code phan sinh ra va di chuyen cua boss

---
+++ Project_PRU_AlientAttack/Assets/Scripts/Spawner.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Spawner : MonoBehaviour
+{
+
+    [Header("Settings")]
+    [SerializeField] private int enemyCount = 10;
+    [SerializeField] private GameObject testGO;
+
+    [Header("Fixed Delay")]
+    [SerializeField] private float delayBtwSpawns;
+
+    private float _spawnTimer;
+    private int _enemiesSpawned;
+
+    private ObjectPooler _pooler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pooler = GetComponent <ObjectPooler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _spawnTimer = Time.deltaTime;
+        if (_spawnTimer < 0)
+        {
+            _spawnTimer = delayBtwSpawns;
+            if (_enemiesSpawned < enemyCount)
+            {
+                _enemiesSpawned++;
+                SpawnEnemy();
+            }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject newInstance = _pooler.GetInstanceFromPool();
+        newInstance.SetActive(true);
+    }
+}
