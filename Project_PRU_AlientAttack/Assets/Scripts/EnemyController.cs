33c92aa8e35ea9dd413c629f1522a84a146c3403
Nguyen Xuan Ly <nguyenxuanly911@gmail.com>
2023-03-05 12:33:46 +0700

    update script enemycontroller

--- Project_PRU_AlientAttack/Assets/Scripts/EnemyController.cs
+++ Project_PRU_AlientAttack/Assets/Scripts/EnemyController.cs
@@ -5,38 +5,15 @@ using UnityEngine.TestTools;
 
 public class EnemyController : MonoBehaviour
 {
-    public int hp;
+    public int maxHealth;
+    public int health;
     public float speed;
     public int gold;
-    private float moveRange;
 
     public Rigidbody2D rb;
 
-    // Start is called before the first frame update
-    [SerializeField] private Vector2 moveRandom;
-    // Start is called before the first frame update
-    void Start()
+   public void Init()
     {
-        moveRandom = new Vector2(Random.Range(-transform.position.x, transform.position.x + moveRange),
-                                        Random.Range(-transform.position.y, transform.position.y + moveRange));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
-        if (currentPos == moveRandom)
-        {
-            GeneratePoint();
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, moveRandom, speed * Time.deltaTime);
-        }
-    }
-
-    private void GeneratePoint()
-    {
-        moveRandom = new Vector2(Random.Range(-5, 5), Random.Range(-4, 4));
+        health = maxHealth;
     }
 }
