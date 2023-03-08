97e6a17ee51759052319afc57b4a7cc89ee045f0
Nguyen Xuan Ly <nguyenxuanly911@gmail.com>
2023-03-08 22:28:42 +0700

    update

--- Project_PRU_AlientAttack/Assets/Scripts/WaveSpawner.cs
+++ Project_PRU_AlientAttack/Assets/Scripts/WaveSpawner.cs
@@ -13,16 +13,6 @@ public class WaveSpawner : MonoBehaviour
     public Transform spawnPoint;
     private int waveNumber = 1;
 
-    void Awake()
-    {
-        
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
