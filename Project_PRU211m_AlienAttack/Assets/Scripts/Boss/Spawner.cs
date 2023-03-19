540722f90b1eab3f80eb1ba6211c170ed404d828
DuctmHE153518 <ductmhe153518@fpt.edu.vn>
2023-03-19 21:31:44 +0700

    update code quai tru mau

--- Project_PRU211m_AlienAttack/Assets/Scripts/Boss/Spawner.cs
+++ Project_PRU211m_AlienAttack/Assets/Scripts/Boss/Spawner.cs
@@ -6,17 +6,51 @@ using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private float spawnRate = 1f;
-    [SerializeField] private GameObject[] bossPrefabs;
-    [SerializeField] private bool canSpawn = true;
+    public Wave[] waves;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 10f;
+    private int waveIndex = 0;
+
+    public static int EnemiesAlive = 0;
+
+    //[SerializeField] private float spawnRate = 1f;
+    //[SerializeField] private GameObject[] bossPrefabs;
+    //[SerializeField] private bool canSpawn = true;
 
     // Start is called before the first frame update
     private void Start()
     {
-        StartCoroutine(BossSpawner());
+        //StartCoroutine(BossSpawner());
+        //StartCoroutine(SpawnWave());
     }
 
-    private IEnumerator BossSpawner()
+    void Update()
+    {
+        if (EnemiesAlive > 0)
+        {
+            return;
+        }
+
+        /*if (waveIndex == waves.Length)
+        {
+            //gameManager.WinLevel();
+            this.enabled = false;
+        }*/
+
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            return;
+        }
+
+        countdown -= Time.deltaTime;
+
+        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+    }
+
+    /*private IEnumerator BossSpawner()
     {
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
@@ -28,5 +62,25 @@ public class Spawner : MonoBehaviour
 
             Instantiate(bossToSpawn, transform.position, Quaternion.identity);
         }
+    }*/
+
+
+    IEnumerator SpawnWave()
+    {
+        Wave wave = waves[waveIndex];
+        for (int i = 0; i < wave.count; i++)
+        {
+            int rand = UnityEngine.Random.Range(0, wave.enemy.Length);
+            GameObject bossToSpawn = wave.enemy[rand];
+
+            Instantiate(bossToSpawn, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+        waveIndex++;
+    }
+
+    void SpawnEnemy(GameObject[] enemy)
+    {
+        //Instantiate(enemy, transform.position, Quaternion.identity);
     }
 }
